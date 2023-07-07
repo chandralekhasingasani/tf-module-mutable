27578914c76230f771d1cf15f777ab4048712618
chandralekhasingasani <chandralekha.s@nutanix.com>
2023-07-07 22:50:26 +0530

    test

--- data.tf
+++ data.tf
@@ -1,6 +1,5 @@
 data "aws_ami" "ansible-ami" {
-  executable_users = ["self"]
   most_recent      = true
-  name_regex       = "^base-ami-ansible"
+  name_regex       = "base-ami-ansible"
   owners           = ["self"]
 }
