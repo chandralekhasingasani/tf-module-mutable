e081eb6e8ac5c579287c7ccdd88d047d611374aa
chandralekhasingasani <chandralekha.s@nutanix.com>
2023-07-07 23:07:24 +0530

    test

--- main.tf
+++ main.tf
@@ -11,6 +11,14 @@ resource "aws_spot_instance_request" "spot" {
   }
 }
 
+resource "aws_ec2_tag" "example" {
+  count         = var.SPOT_INSTANCE_COUNT
+  resource_id   = element(aws_spot_instance_request.spot.*.spot_instance_id, count.index+1)
+  key           = "Name"
+  value         = "${var.COMPONENT}-${var.ENV}"
+}
+
+
 resource "aws_instance" "instance" {
   count         = var.INSTANCE_COUNT
   ami           = data.aws_ami.ansible-ami.id
