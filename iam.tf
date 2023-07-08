dbbf2e8dc2331fd01570e016b916f3adf2441c5d
chandralekhasingasani <chandralekha.s@nutanix.com>
2023-07-08 09:52:00 +0530

    test

--- iam.tf
+++ iam.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_policy" "policy" {
   count       = var.IAM_INSTANCE_PROFILE ? 1:0
-  name        = "${var.ENV}-${var.COMPONENT}secret-manager-reader-policy"
+  name        = "${var.ENV}-${var.COMPONENT}-secret-manager-reader-policy"
   path        = "/"
 
   policy = jsonencode({
@@ -31,6 +31,7 @@ resource "aws_iam_policy" "policy" {
 }
 
 resource "aws_iam_role" "role" {
+  count       = var.IAM_INSTANCE_PROFILE ? 1:0
   name = "${var.COMPONENT}-${var.ENV}"
 
   # Terraform's "jsonencode" function converts a
@@ -51,6 +52,13 @@ resource "aws_iam_role" "role" {
 }
 
 resource "aws_iam_role_policy_attachment" "policy-role-attach" {
-  role       = aws_iam_role.role.name
-  policy_arn = aws_iam_policy.policy.arn
+  count       = var.IAM_INSTANCE_PROFILE ? 1:0
+  role       = aws_iam_role.role.*.name[0]
+  policy_arn = aws_iam_policy.policy.*.arn[0]
+}
+
+resource "aws_iam_instance_profile" "test_profile" {
+  count       = var.IAM_INSTANCE_PROFILE ? 1:0
+  name = "${var.COMPONENT}-${var.ENV}"
+  role = aws_iam_role.role.*.name[0]
 }
